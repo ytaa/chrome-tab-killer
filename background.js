119c00621294815605410fc125165122d4315a92
ytaa <trdo1009@gmail.com>
2023-06-20 15:32:36 +0200

    Add random idle time support

--- background.js
+++ background.js
@@ -1,17 +1,53 @@
-import { default_idle_time, defualt_tab_title } from "./defaults.js";
+import { default_idle_time, default_idle_time_min, default_idle_time_max, default_idle_time_is_random, defualt_tab_title } from "./defaults.js";
 
 var tab_title = defualt_tab_title;
+var idle_time_min = default_idle_time_min;
+var idle_time_max = default_idle_time_max;
+var idle_time_is_random = default_idle_time_is_random;
+
+function service_worker_set_random_idle_interval(idle_time_min, idle_time_max) {
+	const min = Number(idle_time_min);
+	const max = Number(idle_time_max);
+
+	const idle_time_random = Math.floor(Math.random() * (max - min) + min);
+	chrome.idle.setDetectionInterval(idle_time_random);
+
+	return idle_time_random;
+}
 
 function service_worker_update_options() {
 	chrome.storage.local.get(
-		{ idle_time: default_idle_time, tab_title: defualt_tab_title},
+		{ 
+			idle_time: default_idle_time, 
+			idle_time_is_random: default_idle_time_is_random,
+			idle_time_min: default_idle_time_min, 
+			idle_time_max: default_idle_time_max, 
+			tab_title: defualt_tab_title 
+		},
 		(items) => {
-			chrome.idle.setDetectionInterval(Number(items.idle_time));
-			tab_title = items.tab_title;
-
 			console.log("applying settings");
-			console.log("idle time: " + items.idle_time);
+
+			tab_title = items.tab_title;
+			idle_time_is_random = items.idle_time_is_random;
+			idle_time_min = items.idle_time_min;
+			idle_time_max = items.idle_time_max;
+
 			console.log("tab title: " + tab_title);
+			console.log("random idle time: " + idle_time_is_random);
+			
+			if(idle_time_is_random)
+			{
+				console.log("idle time min: " + idle_time_min);
+				console.log("idle time max: " + idle_time_max);
+
+				const idle_time_random = service_worker_set_random_idle_interval(idle_time_min, idle_time_max);
+				console.log("idle time random: " + idle_time_random);
+			}
+			else
+			{
+				chrome.idle.setDetectionInterval(Number(items.idle_time));
+				console.log("idle time: " + items.idle_time);
+			}
 		}
 	);
 }
@@ -21,16 +57,22 @@ function service_worker_init() {
 		async function(state) {
 			if (state === "active") {
 				// Computer not locked, user active
-				console.log("State: active")
+				console.log("state: active")
 			} 
 			else {
 				// Computer is locked, or the user was inactive for given time
-				console.log("State: inactive")
+				console.log("state: inactive")
 				
 				const [tab] = await chrome.tabs.query({ title: tab_title });
 				if(tab){
-					console.log("Removing tab with id: ", tab.id);
+					console.log("removing tab with id: ", tab.id);
 					chrome.tabs.remove(tab.id);
+				}
+
+				if(idle_time_is_random)
+				{
+					const idle_time_random = service_worker_set_random_idle_interval(idle_time_min, idle_time_max);
+					console.log("new idle time random: ", idle_time_random);
 				}
 			}
 		}
--- options.js
+++ options.js
@@ -1,17 +1,31 @@
-import { default_idle_time, defualt_tab_title } from "./defaults.js";
+import { default_idle_time, default_idle_time_min, default_idle_time_max, default_idle_time_is_random, defualt_tab_title } from "./defaults.js";
+
+const validateIdleTime = (elementId) => {
+	var time = document.getElementById(elementId).value;
+	if(time < 15){
+		time = 15;
+		document.getElementById(elementId).value = 15;
+	}
+	return time;
+}
 
 // Saves options to chrome.storage
 const saveOptions = () => {
-	var idle_time = document.getElementById('idle_time').value;
-	if(idle_time < 15){
-		idle_time = 15;
-		document.getElementById('idle_time').value = 15;
-	}
+	const idle_time = validateIdleTime('idle_time');
+	const idle_time_min = validateIdleTime('idle_time_min');
+	const idle_time_max = validateIdleTime('idle_time_max');
+	const idle_time_is_random = document.getElementById('idle_time_is_random').checked;
 
 	const tab_title = document.getElementById('tab_title').value;
 
 	chrome.storage.local.set(
-		{ idle_time: idle_time, tab_title: tab_title },
+		{ 
+			idle_time: idle_time, 
+			idle_time_is_random: idle_time_is_random,
+			idle_time_min: idle_time_min, 
+			idle_time_max: idle_time_max, 
+			tab_title: tab_title 
+		},
 		() => {
 			// Update status to let user know options were saved.
 			const status = document.getElementById('status');
@@ -27,13 +41,41 @@ const saveOptions = () => {
 
 const restoreOptions = () => {
 	chrome.storage.local.get(
-		{ idle_time: default_idle_time, tab_title: defualt_tab_title },
+		{ 
+			idle_time: default_idle_time, 
+			idle_time_is_random: default_idle_time_is_random,
+			idle_time_min: default_idle_time_min, 
+			idle_time_max: default_idle_time_max, 
+			tab_title: defualt_tab_title 
+		},
 		(items) => {
-			document.getElementById('idle_time').value = items.idle_time;
 			document.getElementById('tab_title').value = items.tab_title;
+			document.getElementById('idle_time_is_random').value = items.idle_time_is_random;
+			document.getElementById('idle_time').value = items.idle_time;
+			document.getElementById('idle_time_min').value = items.idle_time_min;
+			document.getElementById('idle_time_max').value = items.idle_time_max;
 		}
 	);
 };
 
+const idleTimeIsRandomOnChange = () => {
+
+	if(document.getElementById('idle_time_is_random').checked)
+	{
+		// random
+		document.getElementById('idle_time_tr').hidden = true;
+		document.getElementById('idle_time_min_tr').hidden = false;
+		document.getElementById('idle_time_max_tr').hidden = false;
+	}
+	else
+	{
+		document.getElementById('idle_time_tr').hidden = false;
+		document.getElementById('idle_time_min_tr').hidden = true;
+		document.getElementById('idle_time_max_tr').hidden = true;
+	}
+};
+
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save').addEventListener('click', saveOptions);
+
+document.getElementById('idle_time_is_random').addEventListener('click', idleTimeIsRandomOnChange);
